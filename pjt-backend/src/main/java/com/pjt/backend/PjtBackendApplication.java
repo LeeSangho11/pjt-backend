909fbe0e32544291a79bef13dbdb6bf12522a813
LeeSangho <leeesh45@gmail.com>
2024-04-16 17:32:47 +0900

    initial Commit

---
+++ pjt-backend/src/main/java/com/pjt/backend/PjtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pjt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjtBackendApplication.class, args);
+	}
+
+}
---
+++ pjt-backend/src/test/java/com/pjt/backend/PjtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
